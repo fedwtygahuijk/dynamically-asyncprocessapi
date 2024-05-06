5dcfdb5f3732d96772c5da530a2090b61ee8a3ce
fedwtygahuijk <csjcbozoglan277@hotmail.com>
2024-05-06 19:58:01 +0700

    demo sdk

--- index.js
+++ index.js
@@ -1,8 +1,38 @@
-function minCostClimbingStairs(cost) {
-  const n = cost.length;
-  const dp = new Array(n + 1).fill(0);
-  for (let i = 2; i <= n; i++) {
-    dp[i] = Math.min(dp[i - 1] + cost[i - 1], dp[i - 2] + cost[i - 2]);
+function longestIncreasingPath(matrix) {
+  if (matrix.length === 0) return 0;
+  const rows = matrix.length;
+  const cols = matrix[0].length;
+  const dp = Array.from(Array(rows), () => Array(cols).fill(0));
+  let longestPath = 0;
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < cols; j++) {
+      longestPath = Math.max(longestPath, dfs(matrix, i, j, dp));
+    }
   }
-  return dp[n];
+  return longestPath;
+  function dfs(matrix, i, j, dp) {
+    if (dp[i][j] !== 0) return dp[i][j];
+    const dirs = [
+      [0, 1],
+      [0, -1],
+      [1, 0],
+      [-1, 0],
+    ];
+    let maxPath = 1;
+    for (const dir of dirs) {
+      const newRow = i + dir[0];
+      const newCol = j + dir[1];
+      if (
+        newRow >= 0 &&
+        newRow < matrix.length &&
+        newCol >= 0 &&
+        newCol < matrix[0].length &&
+        matrix[newRow][newCol] > matrix[i][j]
+      ) {
+        maxPath = Math.max(maxPath, 1 + dfs(matrix, newRow, newCol, dp));
+      }
+    }
+    dp[i][j] = maxPath;
+    return maxPath;
+  }
 }
