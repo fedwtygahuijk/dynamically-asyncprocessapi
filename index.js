9e3239de5aff13ba91ac76974187a97c2e56a474
fedwtygahuijk <csjcbozoglan277@hotmail.com>
2024-05-07 20:39:37 +0700

    hello world commit

--- index.js
+++ index.js
@@ -1,38 +1,19 @@
-function longestIncreasingPath(matrix) {
-  if (matrix.length === 0) return 0;
-  const rows = matrix.length;
-  const cols = matrix[0].length;
-  const dp = Array.from(Array(rows), () => Array(cols).fill(0));
-  let longestPath = 0;
-  for (let i = 0; i < rows; i++) {
-    for (let j = 0; j < cols; j++) {
-      longestPath = Math.max(longestPath, dfs(matrix, i, j, dp));
+function getHint(secret, guess) {
+  let bulls = 0;
+  let cows = 0;
+  const map = new Map();
+  for (let i = 0; i < secret.length; i++) {
+    if (secret[i] === guess[i]) {
+      bulls++;
+    } else {
+      map.set(secret[i], (map.get(secret[i]) || 0) + 1);
     }
   }
-  return longestPath;
-  function dfs(matrix, i, j, dp) {
-    if (dp[i][j] !== 0) return dp[i][j];
-    const dirs = [
-      [0, 1],
-      [0, -1],
-      [1, 0],
-      [-1, 0],
-    ];
-    let maxPath = 1;
-    for (const dir of dirs) {
-      const newRow = i + dir[0];
-      const newCol = j + dir[1];
-      if (
-        newRow >= 0 &&
-        newRow < matrix.length &&
-        newCol >= 0 &&
-        newCol < matrix[0].length &&
-        matrix[newRow][newCol] > matrix[i][j]
-      ) {
-        maxPath = Math.max(maxPath, 1 + dfs(matrix, newRow, newCol, dp));
-      }
+  for (let i = 0; i < guess.length; i++) {
+    if (secret[i] !== guess[i] && map.has(guess[i]) && map.get(guess[i]) > 0) {
+      cows++;
+      map.set(guess[i], map.get(guess[i]) - 1);
     }
-    dp[i][j] = maxPath;
-    return maxPath;
   }
+  return `${bulls}A${cows}B`;
 }
